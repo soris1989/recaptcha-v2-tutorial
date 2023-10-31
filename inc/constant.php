60fb58a1d4ccb875107e33bef63243b960ea0691
DESKTOP-SK1K6UJ\ori <ori.sarah.sa@gmail.com>
2023-10-31 23:42:36 +0200

    starting point

--- inc/constant.php
+++ inc/constant.php
@@ -1,9 +1,5 @@
 <?php
 //reCAPTCHA Configuration - go to Google and get the below keys http://www.google.com/recaptcha/admin
-// define('SITE_KEY','6LfWZGwoAAAAAM5Ukag9sOzsnzLpH9kCDZHgKAtq'); 
-// define('SECRET_KEY','6LfWZGwoAAAAAOg6ehYb6wGGzBjpwoZ71w4Roiep');
-
-// reCAPTCHA test keys
 define('SITE_KEY','6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI'); 
 define('SECRET_KEY','6LeIxAcTAAAAAGG-vFI1TnRWxMZNFuojJ4WifJWe');
 ?>
